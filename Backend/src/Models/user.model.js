9edeb902312b1970207ed38b24f9279736aa3f45
swapnil <sutarswapnil322@gmail.com>
2025-04-16 00:12:43 +0530

    Add user model schema with validation and timestamps

---
+++ Backend/src/Models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+    },
+    profilePic: {
+      type: String,
+      default: ""
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", UserSchema);
