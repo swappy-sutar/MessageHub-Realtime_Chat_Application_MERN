912b00b83be9f20e2f37e630bea957861f251077
swapnil <sutarswapnil322@gmail.com>
2025-04-16 10:55:50 +0530

    Add authentication middleware to verify JWT and authorize users

---
+++ Backend/src/Middlewares/auth.middleware.js
@@ -0,0 +1,43 @@
+import JWT from "jsonwebtoken";
+import { User } from "../Models/user.model.js";
+
+const auth = async (req, res, next) => {
+  try {
+    const token =
+      req.header("Authorization")?.replace("Bearer ", "") ||
+      req.body.token ||
+      req.header("token") ||
+      req.cookies?.token;
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token is missing. Please login.",
+      });
+    }
+
+    const decodeToken = JWT.verify(token, process.env.JWT_SECRET_KEY);
+
+    const user = await User.findById(decodeToken.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized: User not found",
+      });
+    }
+
+    req.user = user;
+
+    next();
+
+  } catch (error) {
+    console.error(`Error in authenticate user: ${error.message}`);
+    return res.status(401).json({
+      success: false,
+      message: `Internal server error: ${error.message}`,
+    });
+  }
+};
+
+export { auth };
