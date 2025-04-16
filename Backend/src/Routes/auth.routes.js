9e77f8016aeae97465ab78cde3ee4f9603b6874c
swapnil <sutarswapnil322@gmail.com>
2025-04-16 11:16:33 +0530

    Add update profile route with authentication middleware

--- Backend/src/Routes/auth.routes.js
+++ Backend/src/Routes/auth.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { signupUser, loginUser} from "../Controllers/auth.controller.js";
+import { signupUser, loginUser, updateProfile} from "../Controllers/auth.controller.js";
+import { auth } from "../Middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+router.put("/update-profile", auth, updateProfile);
 
 
 export default router;
