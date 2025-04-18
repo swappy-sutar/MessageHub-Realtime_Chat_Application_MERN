9769e0950d12fdb511619a8bbb5a4eb3b488eb04
swapnil <sutarswapnil322@gmail.com>
2025-04-18 13:08:52 +0530

    Add CORS support to the application

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,19 @@
 import express from "express";
 import fileUpload from "express-fileupload";
 import { cloudinaryConnect } from "./Config/cloudinary.config.js";
+import cors from "cors";
 
 const app = express();
 
+const corsOptions = {
+  origin: ["https://chat-app-frontend-1.vercel.app", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
 app.use(
   fileUpload({
