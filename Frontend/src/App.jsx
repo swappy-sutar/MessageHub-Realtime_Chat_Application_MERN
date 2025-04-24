d227b16cdc106587e415b7cda3f867f5fa70714e
swapnil <sutarswapnil322@gmail.com>
2025-04-25 00:41:51 +0530

    Refactor App component to implement routing and authentication checks with Loader

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,55 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from "react";
+import { Routes,Route, Navigate } from 'react-router-dom'
 import './App.css'
+import Navbar from './Components/Navbar'
+import HomePage from './pages/HomePage'
+import SignupPage from './pages/SingupPage';
+import LoginPage from './pages/LoginPage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
+import { useAuthStore } from './utils/useAuthStore'
+import {Loader} from "lucide-react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
 
   return (
-   <>
-   </>
-  )
+    <>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignupPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+    </>
+  );
 }
+
 
 export default App
