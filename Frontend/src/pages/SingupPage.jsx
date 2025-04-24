5d0e4bac41d6f3aec297094fad95d8c2a4b7e019
swapnil <sutarswapnil322@gmail.com>
2025-04-25 00:42:32 +0530

    Add SignupPage component with form validation and password visibility toggle

---
+++ Frontend/src/pages/SingupPage.jsx
@@ -0,0 +1,229 @@
+import React, { useState } from "react";
+import { useAuthStore } from "../utils/useAuthStore";
+import { Eye, EyeOff, Mail, Lock, MessageSquare, User } from "lucide-react";
+import { Link } from "react-router-dom";
+import AuthImagePattern from "../Components/AuthImagePattern";
+import { toast } from "react-hot-toast";
+function SignupPage() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const { signup, isSigningUp } = useAuthStore();
+
+  const validateForm = () => {
+    const { firstName, lastName, email, password, confirmPassword } = formData;
+
+    if (!firstName.trim()) return toast.error("First name is required.");
+    if (!lastName.trim()) return toast.error("Last name is required.");
+    if (!email.trim()) return toast.error("Email is required.");
+    if (!/\S+@\S+\.\S+/.test(email))
+      return toast.error("Invalid email format.");
+    if (!password) return toast.error("Password is required.");
+    if (!confirmPassword) return toast.error("Confirm password is required.");
+    if (password.length < 6)
+      return toast.error("Password must be at least 6 characters long.");
+    if (password !== confirmPassword)
+      return toast.error("Passwords do not match.");
+
+    return true;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const validated = validateForm();
+    if (validated === true) {
+      await signup(formData);
+    }
+  };
+
+  return (
+    <div className="min-h-screen grid lg:grid-cols-2 ">
+      {/* Left Section */}
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
+          {/* Logo and Heading */}
+          <div className="text-center">
+            <div className="size-12 mx-auto mb-4 rounded-xl bg-primary/10 flex items-center justify-center transition-all duration-200">
+              <MessageSquare className="size-6 text-primary" />
+            </div>
+            <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+            <p className="text-base-content/60">
+              Get started with your free account
+            </p>
+          </div>
+
+          {/* Form */}
+          <form onSubmit={handleSubmit} className="space-y-3">
+            {/* First Name */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">First Name</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <User className="size-5 text-gray-400" />
+                </div>
+                <input
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={(e) =>
+                    setFormData({ ...formData, firstName: e.target.value })
+                  }
+                  className="input input-bordered pl-11 w-full"
+                  placeholder="First Name"
+                />
+              </div>
+            </div>
+
+            {/* Last Name */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Last Name</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <User className="size-5 text-gray-400" />
+                </div>
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={(e) =>
+                    setFormData({ ...formData, lastName: e.target.value })
+                  }
+                  className="input input-bordered pl-11 w-full"
+                  placeholder="Last Name"
+                />
+              </div>
+            </div>
+
+            {/* Email */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <Mail className="size-5 text-gray-400" />
+                </div>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                  className="input input-bordered pl-11 w-full"
+                  placeholder="Email"
+                />
+              </div>
+            </div>
+
+            {/* Password */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Password</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <Lock className="size-5 text-gray-400" />
+                </div>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                  className="input input-bordered pl-11 w-full"
+                  placeholder="Password"
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOff className="size-5 text-gray-400" />
+                  ) : (
+                    <Eye className="size-5 text-gray-400" />
+                  )}
+                </button>
+              </div>
+            </div>
+
+            {/* Confirm Password */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Confirm Password</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <Lock className="size-5 text-gray-400" />
+                </div>
+                <input
+                  type={showConfirmPassword ? "text" : "password"}
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={(e) =>
+                    setFormData({
+                      ...formData,
+                      confirmPassword: e.target.value,
+                    })
+                  }
+                  className="input input-bordered pl-11 w-full"
+                  placeholder="Confirm Password"
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
+                  {showConfirmPassword ? (
+                    <EyeOff className="size-5 text-gray-400" />
+                  ) : (
+                    <Eye className="size-5 text-gray-400" />
+                  )}
+                </button>
+              </div>
+            </div>
+
+            {/* Submit Button */}
+            <button
+              type="submit"
+              className="btn btn-primary w-full"
+              disabled={isSigningUp}
+            >
+              {isSigningUp ? "Creating Account..." : "Sign Up"}
+            </button>
+          </form>
+          <div>
+            <p className="text-center text-base-content/60">
+              Already have an account?{" "}
+              <Link to="/login" className="link link-primary font-semibold">
+                Log In
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Right Section */}
+      <AuthImagePattern
+        title="Join our community"
+        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+      />
+    </div>
+  );
+}
+
+export default SignupPage;
