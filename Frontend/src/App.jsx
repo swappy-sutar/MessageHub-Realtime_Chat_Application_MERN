bdd1e00be6f701d8e08d08d37446d8fffef72e4a
swapnil <sutarswapnil322@gmail.com>
2025-04-26 01:50:06 +0530

    Refactor App component for improved readability and structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,55 +1,61 @@
 import { useState, useEffect } from "react";
-import { Routes,Route, Navigate } from 'react-router-dom'
-import './App.css'
-import Navbar from './Components/Navbar'
-import HomePage from './pages/HomePage'
-import SignupPage from './pages/SingupPage';
-import LoginPage from './pages/LoginPage';
-import SettingsPage from './pages/SettingsPage';
-import ProfilePage from './pages/ProfilePage';
-import { useAuthStore } from './utils/useAuthStore'
-import {Loader} from "lucide-react"
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SingupPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./utils/useAuthStore";
+import { Loader } from "lucide-react";
 
 function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
-
+ 
   useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
+   const data = checkAuth();
+   console.log("checkAuth Data:", data);
+   
+  }, []);
 
   if (isCheckingAuth && !authUser) {
     return (
-      <div className="flex items-center justify-center h-screen">
-        <Loader className="size-10 animate-spin" />
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <Loader className="size-10 text-primary animate-spin" />
       </div>
     );
   }
 
   return (
-    <>
+    <div className=" h-screen w-full">
       <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={authUser ? <HomePage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/signup"
-          element={!authUser ? <SignupPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route
-          path="/profile"
-          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
-        />
-      </Routes>
-    </>
+
+      <main className="pt-6 px-4 ">
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignupPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/settings"
+            element={authUser ? <SettingsPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/profile"
+            element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
-
-export default App
+export default App;
