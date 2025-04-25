d02b33154eaaadec1e3d9a9a4944985f88b4b6e9
swapnil <sutarswapnil322@gmail.com>
2025-04-26 01:49:52 +0530

    Add logout and checkAuth routes to auth.routes.js

--- Backend/src/Routes/auth.routes.js
+++ Backend/src/Routes/auth.routes.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { signupUser, loginUser, updateProfile} from "../Controllers/auth.controller.js";
+import {
+  signupUser,
+  loginUser,
+  updateProfile,
+  checkAuth,
+  logout,
+} from "../Controllers/auth.controller.js";
 import { auth } from "../Middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -7,6 +13,9 @@ const router = express.Router();
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.put("/update-profile", auth, updateProfile);
+router.post("/logout", logout);
+
+router.get("/check-auth",auth, checkAuth);
 
 
 export default router;
