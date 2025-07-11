bd822b3b7cc518480f534be4f5d7074a1f93f65f
swapnil <sutarswapnil322@gmail.com>
2025-07-11 20:03:21 +0530

    Refactor CORS and body parser setup for improved clarity and performance

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,25 +5,30 @@ import cors from "cors";
 
 const app = express();
 
+// CORS setup
 const corsOptions = {
   origin: ["https://chat-app-frontend-1.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
+// Body parser with limits
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+// File upload
 app.use(
   fileUpload({
     useTempFiles: true,
-    tempFileDir: "/tmp",
+    tempFileDir: "./tmp",
   })
 );
 
+// Cloudinary config
 cloudinaryConnect();
 
+// Routes
 app.get("/", (req, res) => {
   res.json({
     success: true,
