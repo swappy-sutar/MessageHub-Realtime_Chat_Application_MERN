bd5af399ed9b2df13ea3b169e651a9d555fa18cf
swapnil <sutarswapnil322@gmail.com>
2025-07-11 20:03:03 +0530

    Remove cookie setting from generateToken function for cleaner token management

--- Backend/src/Utils/utils.js
+++ Backend/src/Utils/utils.js
@@ -5,13 +5,6 @@ const generateToken = (payload, res) => {
     expiresIn: "2d",
   });
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    maxAge: 2 * 24 * 60 * 60 * 1000, // 2 days
-    sameSite: "strict", // CSRF protection
-    secure: process.env.NODE_ENV === "production", // Set to true in production
-  });
-
   return token;
 };
 
