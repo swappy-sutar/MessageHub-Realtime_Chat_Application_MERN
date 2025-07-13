440af894c044dd3e3618433be5d4b85facd32724
swapnil <sutarswapnil322@gmail.com>
2025-07-14 00:40:35 +0530

    Refactor ProfilePage layout for improved styling and consistency

--- Frontend/src/pages/ProfilePage.jsx
+++ Frontend/src/pages/ProfilePage.jsx
@@ -19,9 +19,9 @@ function ProfilePage() {
   };
 
   return (
-    <div className="h-screen pt-20">
-      <div className="max-w-2xl mx-auto p-4 py-8">
-        <div className="bg-base-300 rounded-xl p-6 space-y-8">
+    <div className="h-[100%] pt-10 overflow-hidden">
+      <div className="max-w-2xl mx-auto p-4 py-1">
+        <div className="bg-base-300 rounded-xl p-6 space-y-5">
           <div className="text-center">
             <h1 className="text-2xl font-semibold ">Profile</h1>
             <p className="mt-2">Your profile information</p>
@@ -29,12 +29,12 @@ function ProfilePage() {
 
           {/* avatar upload section */}
 
-          <div className="flex flex-col items-center gap-4">
+          <div className="flex flex-col items-center gap-3">
             <div className="relative">
               <img
                 src={selectedImg || authUser?.data?.profilePic || avatarLogo}
                 alt="Profile"
-                className="size-32 rounded-full object-cover border-4 "
+                className="size-25 rounded-full object-cover border-4 "
               />
               <label
                 htmlFor="avatar-upload"
@@ -46,7 +46,7 @@ function ProfilePage() {
                   ${isUpdateProfile ? "animate-pulse pointer-events-none" : ""}
                 `}
               >
-                <Camera className="w-5 h-5 text-base-200" />
+                <Camera className="w-4 h-4 text-base-200" />
                 <input
                   type="file"
                   id="avatar-upload"
@@ -68,29 +68,29 @@ function ProfilePage() {
             </p>
           </div>
 
-          <div className="space-y-6">
+          <div className="space-y-5">
             <div className="space-y-1.5">
-              <div className="text-sm text-zinc-400 flex items-center gap-2">
-                <User className="w-4 h-4" />
+              <div className="text-sm font-semibold  text-zinc-400 flex items-center gap-2">
+                <User className="w-4 h-4"  />
                 Full Name
               </div>
-              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+              <p className="px-4 py-2 bg-base-200 rounded-lg border">
                 {authUser?.data?.firstName} {authUser?.data?.lastName}
               </p>
             </div>
 
             <div className="space-y-1.5">
-              <div className="text-sm text-zinc-400 flex items-center gap-2">
+              <div className="text-sm font-semibold text-zinc-400 flex items-center gap-2">
                 <Mail className="w-4 h-4" />
-                Email Address
+                Email Address 
               </div>
-              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+              <p className="px-4 py-2 bg-base-200 rounded-lg border">
                 {authUser?.data?.email}
               </p>
             </div>
           </div>
 
-          <div className="mt-6 bg-base-300 rounded-xl p-6">
+          <div className="mt-3 bg-base-300 rounded-xl p-3">
             <h2 className="text-lg font-medium  mb-4">Account Information</h2>
             <div className="space-y-3 text-sm">
               <div className="flex items-center justify-between py-2 border-b border-zinc-700">
