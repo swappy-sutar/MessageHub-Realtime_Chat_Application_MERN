04de772bb894fd202bbe3c81b797d0a02130f732
swapnil <sutarswapnil322@gmail.com>
2025-07-14 00:40:10 +0530

    Add route for fetching users in sidebar with authentication

--- Backend/src/Routes/message.routes.js
+++ Backend/src/Routes/message.routes.js
@@ -2,10 +2,12 @@ import express from 'express';
 import {
   getMessages,
   sendMessages,
+  getUsersForSidebar,
 } from "../Controllers/message.controller.js";
 import {auth} from "../Middlewares/auth.middleware.js";
 
 const router = express.Router();
+router.get("/users", auth, getUsersForSidebar);
 router.get("/:id", auth, getMessages);
 router.post("/send/:id", auth, sendMessages);
 
