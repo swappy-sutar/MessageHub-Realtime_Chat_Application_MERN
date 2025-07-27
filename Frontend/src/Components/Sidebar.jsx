4bf3c7f8de2c9b5fc5eac428ce8fb864cea10533
swapnil <sutarswapnil322@gmail.com>
2025-07-27 21:16:11 +0530

    refactor: Remove TODO comment for online filter toggle in Sidebar component

--- Frontend/src/Components/Sidebar.jsx
+++ Frontend/src/Components/Sidebar.jsx
@@ -33,7 +33,6 @@ function Sidebar() {
           <Users className="size-6" />
           <span className="font-medium hidden lg:block">Contacts</span>
         </div>
-        {/* TODO: Online filter toggle */}
         <div className="mt-3 hidden lg:flex items-center gap-2">
           <label className="cursor-pointer flex items-center gap-2">
             <input
@@ -50,7 +49,7 @@ function Sidebar() {
         </div>
       </div>
 
-      <div className="overflow-y-auto w-full py-3">
+      <div className="overflow-y-auto w-full ">
         {filteredUsers.map((user) => (
           <button
             key={user._id}
