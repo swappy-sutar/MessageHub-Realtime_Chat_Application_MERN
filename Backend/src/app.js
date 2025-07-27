f4661eb5a25c745dab6d0d5585a2631a8b4465e0
swapnil <sutarswapnil322@gmail.com>
2025-07-27 21:13:51 +0530

    refactor: Improve CORS setup and organize middleware in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,19 +5,18 @@ import cors from "cors";
 
 const app = express();
 
-// CORS setup
+// CORS Options
 const corsOptions = {
   origin: ["https://chat-app-frontend-1.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
-app.use(cors(corsOptions));
 
-// Body parser with limits
+// Middleware
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-// File upload
 app.use(
   fileUpload({
     useTempFiles: true,
@@ -29,16 +28,16 @@ app.use(
 cloudinaryConnect();
 
 // Routes
+import authRoutes from "./Routes/auth.routes.js";
+import userRoutes from "./Routes/user.routes.js";
+import messageRoutes from "./Routes/message.routes.js";
+
 app.get("/", (req, res) => {
   res.json({
     success: true,
     message: "Welcome to the Chat-App backend",
   });
 });
-
-import authRoutes from "./Routes/auth.routes.js";
-import userRoutes from "./Routes/user.routes.js";
-import messageRoutes from "./Routes/message.routes.js";
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
