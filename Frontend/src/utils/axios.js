ab7b63c3273041f281241029e82b628c3365a8fd
swapnil <sutarswapnil322@gmail.com>
2025-08-01 21:18:00 +0530

    add the Node env

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api/v1" : "/api/v1",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api/v1"
+      : "/api/v1",
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
-
-
-})
+});
 
 export { axiosInstance };
